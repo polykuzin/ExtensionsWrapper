5cfee2ad957376da94ab8a81a38c26a7e39a5680
polykuzin <polykuzin@gmail.com>
2022-11-08 05:11:11 +0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ExtensionsWrapper
+
+A description of this package.
---
+++ Sources/ExtensionsWrapper/ExtensionsWrapper.swift
@@ -0,0 +1,6 @@
+public struct ExtensionsWrapper {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
