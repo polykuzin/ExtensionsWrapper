9057bcf834b0a180b327670f6a6ecf6524cd55fa
Pavel Kuzin <polykuzin@gmail.com>
2023-01-11 12:14:42 +0300

    Update ExtensionsWrapperTests.swift

--- Tests/ExtensionsWrapperTests/ExtensionsWrapperTests.swift
+++ Tests/ExtensionsWrapperTests/ExtensionsWrapperTests.swift
@@ -1,11 +1,1 @@
-import XCTest
-@testable import ExtensionsWrapper
 
-final class ExtensionsWrapperTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(ExtensionsWrapper().text, "Hello, World!")
-    }
-}
